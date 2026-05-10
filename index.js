e8140c332b07edd09f140c028dbad1dbf9d30ff5
dayanchyangiyev <dayanchyangiyev2025@gmail.com>
2026-05-10 17:56:04 +0300

    Added error handling in index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // Create the Express server/app object
 const app = express();
 const port = 3000;
 
-console.log("__dirname:", __dirname);
-console.log("__filename:", __filename);
-console.log("process.cwd():", process.cwd());
+const obGlobal = {
+    obErrors: null
+};
+
+
+function initErrors() {
+    const error_path = path.join(__dirname, "public/resurse/json/erori.json");
+    obGlobal.obErrors = require(error_path);
+}
+
+initErrors();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -15,9 +24,58 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Create a simple route
-app.get("/", (req, res) => {
+app.get(["/", "/index", "/home"], (req, res) => {
     res.render("pages/index", {
         title: "Strike-Hub"
+    });
+});
+
+
+
+function renderError(res, identifier, title, text, image) {
+    let errorItem = obGlobal.obErrors.info_erori.find(item => item.identificator == identifier);
+    
+    if (!errorItem || errorItem.status === false) {
+        let defaultError = obGlobal.obErrors.eroare_default;
+        errorItem = {
+            identificator: identifier || "Eroare",
+            titlu: title || defaultError.titlu,
+            text: text || defaultError.text,
+            imagine: image || defaultError.imagine
+        };
+    } else {
+        errorItem = Object.assign({}, errorItem);
+        if (title) errorItem.titlu = title;
+        if (text) errorItem.text = text;
+        if (image) errorItem.imagine = image;
+    }
+    
+    const imagePath = obGlobal.obErrors.cale_baza + errorItem.imagine;
+    const statusCode = typeof identifier === 'number' ? identifier : 400;
+    
+    res.status(statusCode).render("pages/eroare", {
+        title: errorItem.titlu,
+        errorItem: errorItem,
+        imagePath: imagePath
+    });
+}
+
+// This must be the LAST app.get()
+app.get(/(.*)/, (req, res) => {
+    let page = req.params[0];
+    if (page.startsWith('/')) {
+        page = page.substring(1);
+    }
+
+    res.render(`pages/${page}`, function(error, renderedResult) {
+        if (error) {
+            if (error.message.startsWith("Failed to lookup view")) {
+                return renderError(res, 404);
+            } else {
+                return renderError(res);
+            }
+        }
+        res.send(renderedResult);
     });
 });
 
